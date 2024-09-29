c4b9c38d22b138980f192fc90c227630f575f231
vili-georgieva <pincher@tutanota.de>
2024-09-29 22:34:19 +0200

    h

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 <html>
     <head>
         <title>PHP Test</title>
+        <script src="jquery.js"></script>
+        <script>
+        $(function(){
+            $("#login").load("./inc/login.html");
+        });
+        </script>
+        
     </head>
     <body>
         <?php echo '<p>test</p>'; ?>
+        <div id="login"></div>
+        <a href="./inc/login.html">login</a><br>
+        <a href="./inc/hilfe.html">hilfe</a><br>
     </body>
 </html>
